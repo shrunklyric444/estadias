ac528bd0e5832d0bbf509a95d6aa26a88850414c
shrunklyric444 <ph10.omar10@gmail.com>
2024-11-16 12:23:31 -0600

    ADD: estructura de pantalla de Aviso de privacidad

--- vadevia-frontend/src/components/statics/Privacy.js
+++ vadevia-frontend/src/components/statics/Privacy.js
@@ -1,8 +1,84 @@
 
 const Privacy = () => {
     return (
-        <div>    Privacy
-</div>
+        <div className="container-fluid my-5 py-5">
+            <div className="row pt-5 d-flex justify-content-center">
+                <div className="col-md-10">
+                    <div class="card p-5">
+                        <div class="card-body">
+                            <h4 className="text-center">Aviso de privacidad</h4>
+                            <p>De conformidad con lo dispuesto por la Ley Federal de Protección de Datos Personales en Posesión de Particulares, su Reglamento y la normativa relacionada (la “Legislación de Datos Personales”), se emite el presente Aviso de Privacidad en los siguientes términos:</p>
+
+                            <p>1. Identidad, domicilio y servicios del Responsable.</p>
+                            <p>AD TRIP HOLDING S.A.P.I DE C.V. (el “Responsable” o “Nosotros” indistintamente), mejor conocido como “VADEVIA” y/o sus subsidiarias y afiliadas son responsables de la obtención, uso, divulgación y/o almacenamiento de los datos personales (el “Tratamiento”) de los usuarios (“Usted”, el “Titular” o “Usuario”), de su página de internet <a href="https://www.vadevia.com">https://www.vadevia.com</a> (el “Sitio”) y nuestras redes sociales (las “Redes Sociales”). El Responsable tiene domicilio en Cto Int. Melchor Ocampo 193 piso 5, Alcaldía Miguel Hidalgo, C.P. 11300 en la Ciudad de México, México., mismo que está destinado para oír y recibir notificaciones.</p>
+
+                            <p>2. Datos personales recabados y Tratados por el Responsable.</p>
+                            <p>Para las finalidades indicadas en el presente Aviso de Privacidad, el Responsable trata las siguientes categorías de datos personales y datos personales:</p>
+                            <p>Datos de contacto.</p>
+                            <p>Identificadores en línea e interacciones en las Redes Sociales.</p>
+                            <p>El Responsable no recaba datos personales sensibles.</p>
+
+                            <p>3. Finalidades del Tratamiento.</p>
+                            <p>Trataremos los datos personales para las siguientes finalidades primarias y necesarias:</p>
+                            <p>Proporcionarle acceso a el Sitio.</p>
+                            <p>Proveerle nuestros servicios a través del Sitio y las Redes Sociales.</p>
+                            <p>Administrar, dar mantenimiento y mejorar el Sitio y las Redes Sociales y el contenido de las mismas, así como realizar las actividades necesarias para que éstos funcionen de manera correcta y efectiva.</p>
+                            <p>Proporcionar la información solicitada en relación con los servicios, el Sitio y las Redes Sociales, así como gestionar cualesquier comentarios, quejas, aclaraciones e inconformidades relacionados con los mismos.</p>
+                            <p>Comunicarle sobre los cambios en nuestros servicios y, en su caso, los términos aplicables a los mismos.</p>
+                            <p>Estadística y análisis del Sitio de manera anonimizada.</p>
+                            <p>Registro histórico de usuarios.</p>
+                            <p>Atender requerimientos legales de autoridades competentes nacionales y extranjeras y cumplir con las obligaciones derivadas de la relación jurídica entre usted y el Responsable.</p>
+                            <p>Cumplir con cualesquiera disposiciones y obligaciones legales, nacionales o internacionales, aplicables al Responsable, así como para gestionar cualquier proceso judicial de ésta.</p>
+                            <p>Realizar procesos relacionados con restructuras corporativas del Responsable, tales como la fusión, consolidación, venta, liquidación o transferencia de activos.</p>
+
+                            <p>4. Opciones y medios que el Responsable le ofrece para limitar el uso o divulgación de sus datos personales.</p>
+                            <p>En todos aquellos casos legalmente procedentes, usted podrá limitar el uso o divulgación de sus datos personales dirigiendo la solicitud correspondiente a nuestro Oficial de Datos Personales, siguiendo el procedimiento a que se refiere el numeral 6 del presente Aviso de Privacidad.</p>
+
+                            <p>5. Transferencia de Datos Personales.</p>
+                            <p>Sus datos personales podrán ser transferidos dentro del territorio nacional o hacia el extranjero, conforme a lo siguiente:</p>
+                            <p>1.- A autoridades, organismos o entidades gubernamentales, en cumplimiento a o en relación con las obligaciones contempladas en la legislación aplicable al Responsable, así como en cumplimiento de requerimientos efectuados a las mismas.</p>
+                            <p>2.- A autoridades, organismos o entidades gubernamentales, cuando la transferencia sea precisa para el reconocimiento, ejercicio o defensa de un derecho del Responsable en un proceso judicial.</p>
+                            <p>3.- A terceros derivado de una reestructura corporativa del Representante, incluyendo, la fusión, consolidación, venta, liquidación o transferencia de activos.</p>
+                            <p>4.- A terceros, cuando la transferencia sea precisa para el mantenimiento o cumplimiento de la relación jurídica entre usted y el Responsable.</p>
+                            <p>5.- Con base en los demás supuestos establecidos en la Legislación de Datos Personales que no requieren de su consentimiento.</p>
+                            <p>Las transferencias antes mencionadas no requieren de su consentimiento conforme a la Legislación de Datos Personales.</p>
+                            <p>Le informamos que las remisiones nacionales e internacionales de Datos Personales entre el Responsable y sus encargados no requerirán ser informadas.</p>
+
+                            <p>6. Medios para el ejercicio sus derechos.</p>
+                            <p>En todos aquellos casos legalmente procedentes, usted podrá en todo momento y a través del procedimiento establecido en el presente numeral, ejercer los siguientes derechos referentes a sus datos personales: (i) Derechos de Acceso, Rectificación, Cancelación y Oposición (“Derechos ARCO”); (ii) Revocar el consentimiento otorgado al Responsable para el tratamiento de sus datos personales; (iii) Limitar el uso o divulgación de sus datos personales; y (iv) Manifestar su negativa para el tratamiento de sus datos personales con respecto a las finalidades secundarias y accesorias antes mencionadas.</p>
+                            <p>Para el ejercicio de dichos derechos referentes a sus datos personales deberá presentar ante el Responsable la solicitud correspondiente, mediante un escrito libre debidamente firmado y dirigido a nuestro Oficial de Datos Personales, entregándolo en el domicilio mencionado en el numeral 1 del presente Aviso de Privacidad o enviándolo a la dirección de correo electrónico: <a href="mailto:hola@vadevia.com">hola@vadevia.com</a>, estableciendo como asunto “Ejercicio de derechos sobre datos personales”.</p>
+
+                            <p>La solicitud deberá cumplir con los requisitos establecidos en la Legislación de Datos Personales vigente y deberá contener y acompañar lo siguiente: su nombre y domicilio u otro medio para comunicarle la respuesta a su solicitud; en caso de que la solicitud se haya enviado mediante el correo electrónico antes mencionado, el Responsable podrá responder por el mismo medio, aún y cuando usted no haya indicado dicho medio como un método para comunicarle la respuesta a su solicitud; copia de los documentos que acrediten su identidad (identificación oficial vigente con fotografía) o, en su caso, la representación legal, conforme a los términos establecidos en la Legislación de Datos Personales; en su caso, la descripción clara y precisa del o los derechos que desea ejercer; en el caso de solicitudes de rectificación de datos personales, las modificaciones a realizarse y aportar la documentación que sustente su petición; y cualquier otro elemento o documento que facilite la localización de sus datos personales.</p>
+
+                            <p>Nuestro Oficial de Datos Personales dará trámite a las solicitudes de los titulares para el ejercicio de sus derechos referentes a datos personales en un plazo no mayor a 20 (veinte) días hábiles contados a partir de su recepción y realizará requerimientos o subsanará conforme a los tiempos de la Legislación de Datos Personales. El Responsable podrá ampliar este plazo hasta por 20 (veinte) días hábiles más, cuando el caso lo amerite, previa notificación. En caso de que su solicitud resulte procedente, la misma se hará efectiva dentro de los 15 (quince) días hábiles siguientes de la respuesta por parte de nuestro Oficial de Datos Personales.</p>
+
+                            <p>En caso de que el Responsable, como parte de la respuesta a la solicitud correspondiente, deba proporcionarle Datos Personales o deba expedirle copias de documentos, documentos originales o copias certificadas referentes a los mismos, el Responsable los pondrá a su disposición en el domicilio mencionado en el numeral 1 del presente Aviso de Privacidad, previa acreditación de la identidad y personalidad del titular o su representante legal.</p>
+
+                            <p>Según se establece anteriormente, nuestro Oficial de Datos Personales está a cargo de tramitar cualesquier solicitudes de los titulares para el ejercicio de sus derechos referentes a datos personales y sus datos de contacto son los siguientes:</p>
+                            <p>a) Domicilio: Cto. Int. Melchor Ocampo 193 piso 5, Alcaldía Miguel Hidalgo, C.P. 11000 en la Ciudad de México, México.</p>
+                            <p>b) Correo electrónico: <a href="mailto:hola@vadevia.com">hola@vadevia.com</a></p>
+
+                            <p>El Responsable desea mantener actualizados sus datos personales en todo momento. Por lo anterior, solicitamos su cooperación para comunicarnos oportunamente cualquier cambio que debamos tener en cuenta, para incorporarlo a nuestras bases de datos. Usted garantiza y responde, en cualquier caso, de la veracidad, exactitud, vigencia y autenticidad de los datos personales facilitados.</p>
+
+                            <p>7. Medios que permiten recabar datos personales de manera automática.</p>
+                            <p>Le informamos que el Sitio hace uso de cookies y otras tecnologías, del Responsable y de terceros, a través de las cuales es posible monitorear automáticamente su comportamiento con la finalidad de que usted pueda navegar el Sitio y hacer uso de sus funciones, brindarle nuestros servicios, recordar sus preferencias, otorgarle una experiencia adecuada durante el uso de los mismos, analizar el funcionamiento de Sitio y adaptar y mejorar el mismo, nuestros servicios y contenidos y la interacción de los usuarios con los mismos, así como para el ofrecimiento de productos y servicios basados en sus preferencias.</p>
+
+                            <p>Los datos personales que se recaban a través de estas tecnologías son: dirección IP, sitios web y secciones de los mismos que usted visita desde el Sitio o antes del Sitio, características de navegador, sistema operativo, preferencias de idiomas, links a URLs (localizadores de recursos uniforme) de terceros seleccionados en el Sitio, información sobre acciones realizadas en nuestro Sitio, fechas y horas de las visitas al Sitio, secciones, páginas o contenido consultadas en el mismo. No se tiene el propósito de utilizar dichos datos personales para identificar en lo personal a usuario alguno.</p>
+
+                            <p>Estas tecnologías pueden deshabilitarse accediendo a la(s) opción(es) de Privacidad y/o Seguridad ubicada(s) en la(s) sección(es) de Opciones, Herramientas, Preferencias de Internet o funciones similares del navegador de Internet que esté utilizando; sin embargo, al desactivarlas debe tomar en cuenta que dicha acción podría provocar que no sea capaz de obtener el funcionamiento total que el Sitio pudiera ofrecerle.</p>
+
+                            <p>8. Cambios al Aviso de Privacidad.</p>
+                            <p>El presente Aviso de Privacidad puede sufrir modificaciones, cambios o actualizaciones derivadas de nuevos requerimientos legales, de nuestras propias necesidades por los productos o servicios que ofrecemos, de nuestras prácticas, programas o políticas de privacidad, de cambios en nuestro modelo de negocio, o por otras causas.</p>
+
+                            <p>Nos comprometemos a mantenerlo informado sobre los cambios que pueda sufrir el presente Aviso de Privacidad, a través del Sitio, en la sección correspondiente de Aviso de Privacidad.</p>
+
+                            <p>Actualizado en abril de 2024.</p>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
